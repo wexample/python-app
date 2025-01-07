cd7fa62830202aaf38ee1ff0a1ee233006abd9d0
weeger <romain.weeger@gmail.com>
2025-01-07 17:24:03 +0100

    Rework setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,3 @@
-from setuptools import setup, find_packages
+from wexample_helpers.helpers.package import package_minimal_setup
 
-setup(
-    name='wexample-app',
-    version=open('version.txt').read(),
-    author='weeger',
-    author_email='contact@wexample.com',
-    description='Helpers for building Python app or cli.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/wexample/python-app',
-    packages=find_packages(),
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    install_requires=[
-        'python-dotenv',
-        'pydantic',
-        'wexample-filestate',
-        'wexample-helpers',
-        'wexample-prompt',
-        'wexample-wex-addon-app',
-    ],
-    python_requires='>=3.6',
-)
+package_minimal_setup()
