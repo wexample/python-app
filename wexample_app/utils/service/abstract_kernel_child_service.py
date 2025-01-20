d8cde251a3500f20d3a5412521a31d5a32a643b0
weeger <romain.weeger@gmail.com>
2025-01-20 12:12:04 +0100

    New classes

---
+++ wexample_app/utils/service/abstract_kernel_child_service.py
@@ -0,0 +1,6 @@
+from wexample_app.utils.abstract_kernel_child import AbstractKernelChild
+from wexample_app.utils.service.service_mixin import ServiceMixin
+
+
+class AbstractKernelChildService(ServiceMixin, AbstractKernelChild):
+    pass
---
+++ wexample_app/utils/service/service_mixin.py
@@ -0,0 +1,5 @@
+from wexample_helpers.classes.mixin.has_snake_short_class_name_class_mixin import HasSnakeShortClassNameClassMixin
+
+
+class ServiceMixin(HasSnakeShortClassNameClassMixin):
+    pass
