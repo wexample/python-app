eae9ef514cda452601ee248c6f9e138face270e4
weeger <romain.weeger@gmail.com>
2025-01-22 16:34:46 +0100

    Various syntax

--- wexample_app/utils/service/service_mixin.py
+++ wexample_app/utils/service/service_mixin.py
@@ -1,7 +1,11 @@
+from typing import Optional
+
 from wexample_helpers.classes.mixin.has_class_dependencies import HasClassDependencies
 from wexample_helpers.classes.mixin.has_snake_short_class_name_class_mixin import HasSnakeShortClassNameClassMixin
 from wexample_prompt.mixins.with_prompt_context import WithPromptContext
 
 
 class ServiceMixin(WithPromptContext, HasSnakeShortClassNameClassMixin, HasClassDependencies):
-    pass
+    @classmethod
+    def get_class_name_suffix(cls) -> Optional[str]:
+        return 'Service'
