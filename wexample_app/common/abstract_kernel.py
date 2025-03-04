c95a80649cae86c1edd21ad6deb07763dd9d1b6f
weeger <romain.weeger@gmail.com>
2025-03-04 22:25:21 +0100

    Save addon manager

--- wexample_app/common/abstract_kernel.py
+++ wexample_app/common/abstract_kernel.py
@@ -13,6 +13,7 @@ from wexample_prompt.responses.base_prompt_response import BasePromptResponse
 
 if TYPE_CHECKING:
     from wexample_app.common.command_request import CommandRequest
+    from wexample_app.common.abstract_addon_manager import AbstractAddonManager
 
 
 class AbstractKernel(
@@ -29,10 +30,11 @@ class AbstractKernel(
             ENV_VAR_NAME_APP_ENV
         ]
 
-    def setup(self) -> "AbstractKernel":
+    def setup(self, addons: List[Type["AbstractAddonManager"]]) -> "AbstractKernel":
         self._init_io_manager()
         self._init_workdir(self.entrypoint_path, self.io_manager)
         self._load_env_file(f"{self.workdir.get_resolved()}{self._get_dotenv_file_name()}")
+        self.register_items("addons", addons)
         return self
 
     def _get_command_request_class(self) -> Type["CommandRequest"]:
