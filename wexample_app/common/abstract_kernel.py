039717c4ac9131f767f34f6a91bb4a32935ee26f
weeger <romain.weeger@gmail.com>
2025-03-04 07:04:07 +0100

    Add workdir

--- wexample_app/common/abstract_kernel.py
+++ wexample_app/common/abstract_kernel.py
@@ -3,7 +3,7 @@ from typing import TYPE_CHECKING, Any
 from pydantic import BaseModel, Field
 
 from wexample_app.service.mixins.service_container_mixin import ServiceContainerMixin
-from wexample_prompt.common.io_manager import IoManager
+from wexample_filestate.mixins.with_workdir_mixin import WithWorkdirMixin
 from wexample_prompt.mixins.with_prompt_context import WithPromptContext
 
 if TYPE_CHECKING:
@@ -12,10 +12,15 @@ if TYPE_CHECKING:
 
 class AbstractKernel(
     ServiceContainerMixin,
+    WithWorkdirMixin,
     WithPromptContext,
     BaseModel
 ):
     entrypoint_path: str = Field(description="The main file placed at application root directory")
 
     def model_post_init(self, __context: Any) -> None:
-        self.io = IoManager()
+        self._init_io_manager()
+        self._init_workdir(
+            self.entrypoint_path,
+            self.io_manager
+        )
