d548b04f06ab6d299fe60ae1186d4edd5493d2de
weeger <romain.weeger@gmail.com>
2025-03-04 15:40:55 +0100

    Dead files

--- wexample_app/exception/kernel_exception.py
+++
@@ -1,2 +0,0 @@
-class KernelException(Exception):
-    pass
--- wexample_app/utils/service/abstract_kernel_child_service.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-from wexample_app.utils.abstract_kernel_child import AbstractKernelChild
-from wexample_app.utils.service.service_mixin import ServiceMixin
-
-
-class AbstractKernelChildService(ServiceMixin, AbstractKernelChild, BaseModel):
-    pass
