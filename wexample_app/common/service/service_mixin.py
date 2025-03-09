1d5347f29ef045fbc2c00a151a0fc988c4e56e8e
weeger <romain.weeger@gmail.com>
2025-03-09 17:19:32 +0100

    Add setup

--- wexample_app/common/service/service_mixin.py
+++ wexample_app/common/service/service_mixin.py
@@ -2,10 +2,16 @@ from typing import Optional
 
 from wexample_helpers.classes.mixin.has_class_dependencies import HasClassDependencies
 from wexample_helpers.classes.mixin.has_snake_short_class_name_class_mixin import HasSnakeShortClassNameClassMixin
+from wexample_helpers.classes.mixin.has_two_steps_init import HasTwoStepInit
 from wexample_prompt.mixins.with_prompt_context import WithPromptContext
 
 
-class ServiceMixin(WithPromptContext, HasSnakeShortClassNameClassMixin, HasClassDependencies):
+class ServiceMixin(
+    WithPromptContext,
+    HasSnakeShortClassNameClassMixin,
+    HasTwoStepInit,
+    HasClassDependencies
+):
     @classmethod
     def get_class_name_suffix(cls) -> Optional[str]:
         return 'Service'
