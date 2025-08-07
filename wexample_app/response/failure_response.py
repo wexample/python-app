83172d605459f0c112586b84ab6093c9a409f25b
weeger <romain.weeger@gmail.com>
2025-08-07 11:25:19 +0200

    Failure response

---
+++ wexample_app/response/failure_response.py
@@ -0,0 +1,23 @@
+from typing import TYPE_CHECKING, Optional
+
+from wexample_app.const.types import ResponsePrintable
+from wexample_app.response.abstract_response import AbstractResponse
+
+if TYPE_CHECKING:
+    pass
+
+
+class FailureResponse(AbstractResponse):
+    exception: Optional[Exception] = None
+    message: Optional[str] = None
+
+    def get_printable(self) -> ResponsePrintable:
+        # Build a string representation from exception and/or message
+        if self.exception and self.message:
+            return f"{self.message}: {str(self.exception)}"
+        elif self.exception:
+            return str(self.exception)
+        elif self.message:
+            return self.message
+        else:
+            return "Unknown failure"
