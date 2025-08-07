72a7f6472f67503a940bc379605aa60e7f804d77
weeger <romain.weeger@gmail.com>
2025-08-07 13:33:14 +0200

    Helper

--- wexample_app/response/default_response.py
+++ wexample_app/response/default_response.py
@@ -1,7 +1,7 @@
 from typing import Any, TYPE_CHECKING
 
 from wexample_app.const.types import ResponsePrintable
-from wexample_app.exception.response_invalid_content_exception import ResponseInvalidContentException
+from wexample_app.exception.response_invalid_content_type_exception import ResponseInvalidContentTypeException
 from wexample_app.response.abstract_response import AbstractResponse
 from wexample_helpers.helpers.args import args_is_basic_value
 
@@ -16,7 +16,12 @@ class DefaultResponse(AbstractResponse):
         super().__init__(kernel, **kwargs)
 
         if not args_is_basic_value(self.content):
-            raise ResponseInvalidContentException(content=self.content)
+            from wexample_helpers.const.types import BasicInlineValue
+
+            raise ResponseInvalidContentTypeException(
+                content=self.content,
+                allowed_content_types=BasicInlineValue
+            )
 
     def get_printable(self) -> ResponsePrintable:
         # For now consider every output as a string
