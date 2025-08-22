71be7aa0a9ed535171a27c9f410e43d662f6dfbb
weeger <romain.weeger@gmail.com>
2025-08-22 12:05:01 +0200

    Remove futures

--- wexample_app/common/abstract_kernel.py
+++ wexample_app/common/abstract_kernel.py
@@ -33,7 +33,7 @@ class AbstractKernel(
     def get_expected_env_keys(self) -> list[str]:
         return [ENV_VAR_NAME_APP_ENV]
 
-    def setup(self) -> AbstractKernel:
+    def setup(self) -> "AbstractKernel":
         import os
         from pathlib import Path
 
@@ -47,16 +47,16 @@ class AbstractKernel(
 
         return self
 
-    def _get_command_request_class(self) -> type[CommandRequest]:
+    def _get_command_request_class(self) -> type["CommandRequest"]:
         from wexample_app.common.command_request import CommandRequest
 
         return CommandRequest
 
-    def execute_kernel_command(self, request: CommandRequest) -> AbstractResponse:
+    def execute_kernel_command(self, request: "CommandRequest") -> "AbstractResponse":
         # Save unique root request
         self.root_request = self.root_request if self.root_request else request
 
         return request.execute()
 
-    def execute_kernel_command_and_print(self, request: CommandRequest) -> None:
+    def execute_kernel_command_and_print(self, request: "CommandRequest") -> None:
         self.execute_kernel_command(request=request)
