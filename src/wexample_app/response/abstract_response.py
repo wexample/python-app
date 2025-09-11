4c169a6bec6f61108bfcadf821206f9975c47bc4
weeger <romain.weeger@gmail.com>
2025-09-11 14:32:21 +0200

    Rect

--- src/wexample_app/response/abstract_response.py
+++ src/wexample_app/response/abstract_response.py
@@ -6,8 +6,8 @@ from typing import TYPE_CHECKING
 from wexample_app.common.abstract_kernel_child import AbstractKernelChild
 from wexample_helpers.classes.base_class import BaseClass
 from wexample_helpers.classes.field import public_field
-
 from wexample_helpers.decorator.base_class import base_class
+
 if TYPE_CHECKING:
     from wexample_app.common.abstract_kernel import AbstractKernel
     from wexample_app.const.types import ResponsePrintable
