0add39762633a3698a15120b8fd5c18d5a0160c5
weeger <romain.weeger@gmail.com>
2025-10-04 20:32:05 +0200

    Move wex workdir

--- .wex/python/app_manager/app_workdir.py
+++ .wex/python/app_manager/app_workdir.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 
+from wexample_wex_addon_app.workdir.mixin.app_workdir_mixin import AppWorkdirMixin
 from wexample_wex_addon_dev_python.workdir.python_workdir import PythonWorkdir
-from wexample_wex_core.workdir.project_workdir import ProjectWorkdir
 
 
 class AppWorkdir(PythonWorkdir):
     @classmethod
-    def create_from_config(cls, **kwargs) -> ProjectWorkdir:
+    def create_from_config(cls, **kwargs) -> AppWorkdirMixin:
         print('OK!')
         exit()
