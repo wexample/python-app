0808a6dc2f104a6e6e53aed461a53bd1d0ebd675
weeger <romain.weeger@gmail.com>
2025-10-23 13:50:11 +0200

    App registry write

--- .wex/python/app_manager/app_workdir.py
+++ .wex/python/app_manager/app_workdir.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from wexample_wex_addon_app.workdir.mixin.app_workdir_mixin import AppWorkdirMixin
 from wexample_wex_addon_dev_python.workdir.python_package_workdir import PythonPackageWorkdir
 
 
@@ -9,8 +8,3 @@ class AppWorkdir(PythonPackageWorkdir):
         return [
             'introduction'
         ]
-
-    @classmethod
-    def create_from_config(cls, **kwargs) -> AppWorkdirMixin:
-        print('OK!')
-        exit()
