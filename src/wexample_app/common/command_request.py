30be752c8bf04a9cca21e5d1481952cb26700368
weeger <romain.weeger@gmail.com>
2025-10-26 17:29:29 +0100

    Fix recursion

--- src/wexample_app/common/command_request.py
+++ src/wexample_app/common/command_request.py
@@ -2,10 +2,11 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from wexample_app.common.abstract_kernel_child import AbstractKernelChild
 from wexample_helpers.classes.field import public_field
 from wexample_helpers.classes.private_field import private_field
 from wexample_helpers.decorator.base_class import base_class
+
+from wexample_app.common.abstract_kernel_child import AbstractKernelChild
 
 if TYPE_CHECKING:
     from wexample_helpers.const.types import StringsMatch
