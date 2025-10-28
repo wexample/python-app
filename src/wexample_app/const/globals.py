63ba8b8a25694183a521cb2fa19e19db20dfdebc
weeger <romain.weeger@gmail.com>
2025-10-28 18:03:07 +0100

    Load libraries

--- src/wexample_app/const/globals.py
+++ src/wexample_app/const/globals.py
@@ -7,6 +7,7 @@ WORKDIR_SETUP_DIR: Path = Path(f".{CORE_COMMAND_NAME}")
 
 # filestate: python-constant-sort
 APP_FILE_APP_CONFIG: str = "config.yml"
+APP_FILE_APP_RUNTIME_CONFIG: str = "config.runtime.yml"
 APP_FILE_APP_MANAGER: Path = Path("app-manager")
 APP_PATH_APP_MANAGER: Path = WORKDIR_SETUP_DIR / "python" / "app_manager"
 APP_PATH_BIN_APP_MANAGER: Path = WORKDIR_SETUP_DIR / "bin" / APP_FILE_APP_MANAGER
