755b0b24d186714d7613422b77e264e643bd9381
weeger <romain.weeger@gmail.com>
2025-10-30 11:42:04 +0100

    Outputs rewrite

---
+++ src/wexample_app/const/output.py
@@ -0,0 +1,10 @@
+"""Constants for output handling."""
+
+# Output targets
+OUTPUT_TARGET_STDOUT = "stdout"
+OUTPUT_TARGET_FILE = "file"
+
+# Output formats
+OUTPUT_FORMAT_STR = "str"
+OUTPUT_FORMAT_JSON = "json"
+OUTPUT_FORMAT_YAML = "yaml"
