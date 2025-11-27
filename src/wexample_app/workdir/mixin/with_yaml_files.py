59c9fbaca5e487e07f576050888568182b945c6c
weeger <romain.weeger@gmail.com>
2025-11-27 20:03:59 +0100

    Minor

--- src/wexample_app/workdir/mixin/with_yaml_files.py
+++ src/wexample_app/workdir/mixin/with_yaml_files.py
@@ -2,13 +2,12 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from wexample_filestate.item.file.yaml_file import YamlFile
 from wexample_helpers.classes.base_class import BaseClass
-from wexample_helpers.const.types import PathOrString
 from wexample_helpers.decorator.base_class import base_class
 
 if TYPE_CHECKING:
-    pass
+    from wexample_filestate.item.file.yaml_file import YamlFile
+    from wexample_helpers.const.types import PathOrString
 
 
 @base_class
